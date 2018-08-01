caf5956330268b3bc4812a214af5ea9aaf2c0618
melaudic <melinaaudic@MacBook-Pro-de-Melina.local>
2018-08-01 15:58:14 +0700

    Initialisation du projet

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+  <script src="js/script.js"></script>
+</body>
+
+</html>
