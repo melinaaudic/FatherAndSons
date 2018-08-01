1e8f011d88f311349eaff0d419afaf19b23464ce
melaudic <melinaaudic@MacBook-Pro-de-Melina.local>
2018-08-01 16:54:15 +0700

    Ajout de l'ensemble de l'HTML et des medias

--- index.php
+++ index.php
@@ -5,11 +5,154 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title></title>
+  <!-- style -->
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/app.css">
+  <!-- favicon -->
+  <link rel="icon" href="medias/favicon.ico" />
 </head>
 
 <body>
+  <div class="container-fluid background dark">
+    <header class="wrapper">
+      <div class="row middle-lg between-lg">
+        <img src="medias/logo.png" alt="" class="col-lg-4">
+        <p class="col-lg-2">
+          Magasin Avignon<br>
+          <a href="tel:+33490871175">04.90.87.11.75</a>
+        </p>
+      </div>
+    </header>
+  </div>
+  <main>
+    <section class="page-section container-fluid background image">
+      <div class="wrapper">
+        <div class="row middle-lg center-lg">
+          <h1>Profitez dès aujourd'hui de 15€ de bon d'achat sur votre magasin d'Avignon</h1>
+        </div>
+        <div class="row middle-lg center-lg">
+          <p class="bon-achat">
+            <span>bon d’achat de</span>
+            <span>15€</span>
+            <span>offert*</span>
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="page-section container-fluid wrapper">
+      <div class="row middle-lg center-lg">
+        <p class="col-lg-4">Inscrivez-vous et profitez d’un bon d’achat de 15€ offert* valable dans votre nouveau magasin d’Avignon en centre ville.</p>
+        <form action="" method="get" class="col-lg-4">
+          <input type="email" name="email" placeholder="Adresse e-mail*" required>
+          <input type="submit" value="J'en profite">
+        </form>
+        <p class="col-lg-4">
+          <span>Plus que</span>
+          <span>168h:17m:02s</span>
+          <span>pour en profiter !</span>
+        </p>
+      </div>
+    </section>
+    <section class="page-section container-fluid background dark">
+      <div class="wrapper">
+        <div class="row center-lg">
+          <h2 class="col-lg-4">Collections</h2>
+        </div>
+        <div class="row center-lg">
+          <p class="col-lg-10">Riche d’une expérience de plus de 20 ans dans la mode homme, chacune de nos collections est conçue pour vous proposer des produits de modes, confectionnées dans des matières sélectionnées pour leur qualité incomparable.</p>
+        </div>
+        <div class="row  center-lg">
+          <div class="col-lg-4">
+            <h3>Les costumes</h3>
+          </div>
+          <div class="col-lg-4">
+            <h3>Le casual</h3>
+          </div>
+          <div class="col-lg-4">
+            <h3>Les vestes cuirs</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="page-section container-fluid wrapper">
+      <div class="row center-lg">
+        <h2 class="col-lg-4">Conseils & style</h2>
+      </div>
+      <div class="row  center-lg">
+        <div class="col-lg-4">
+          <h3>Conseils pour prendre soin de sa chemise</h3>
+          <a href="#" class="button">découvrir</a>
+        </div>
+        <div class="col-lg-4">
+          <h3>Comment bien assortir sa cravte à sa chemise ?</h3>
+          <a href="#" class="button">découvrir</a>
+        </div>
+        <div class="col-lg-4">
+          <h3>Comment s'habiller pour un mariage civil ?</h3>
+          <a href="#" class="button">découvrir</a>
+        </div>
+      </div>
+    </section>
+    <section class="page-section container-fluid background light">
+      <div class="wrapper">
+        <div class="row center-lg">
+          <h2 class="col-lg-4">Témoignages</h2>
+        </div>
+      </div>
+    </section>
+    <section class="page-section container-fluid wrapper">
+      <div class="row center-lg">
+        <h2 class="col-lg-4">Ouverture magasin Avignon</h2>
+      </div>
+      <div class="row col-lg-offset-1">
+        <div class="col-lg-4">
+          <h2>Avignon</h2>
+          <p>11 rue Joseph VERNET<br> Avignon <br> 84000 <br> Tel: 04.90.87.11.75</p>
+          <a href="#" class="button">Voir la fiche détaillée</a>
+        </div>
+        <div class="col-lg-6">
+          <p>google maps</p>
+        </div>
+      </div>
+    </section>
+    <section class="page-section container-fluid background light">
+      <div class="wrapper">
+        <div class="row center-lg">
+          <h2 class="col-lg-4">Référénces</h2>
+        </div>
+        <div class="row">
+          <p>images</p>
+        </div>
+      </div>
+    </section>
+    <section class="page-section container-fluid wrapper">
+      <div class="row center-lg">
+        <h2 class="col-lg-4">Vêtement image & Uniforme</h2>
+      </div>
+      <div class="row col-lg-offset-1 middle-lg">
+        <div class="col-lg-4">
+          <p>Cultivez votre image de marque avec élégance ! Et si vous adoptiez une image chic et branchée pour séduire votre clientèle ?</p>
+          <p>Father & Sons, plus de 20 ans de savoir-faire dans le prêt-à-porter au service de vos uniformes professionnels.</p>
+        </div>
+        <div class="col-lg-3">
+          <img src="medias/vetement-image-et-uniforme-homme.jpg" alt="">
+        </div>
+        <div class="col-lg-3">
+          <img src="medias/vetement-image-et-uniforme-femme.jpg" alt="">
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+  <footer class="container-fluid background dark">
+    <div class="row">
+      <div class="col-lg-10">
+        <p>* 15€ Offerts sur dans le magasin d’Avignon uniquement, offre non cumulable.</p>
+        <p>© 2015 Father & Sons et The Other Store - Mentions légales</p>
+      </div>
+    </div>
+  </footer>
   <script src="js/script.js"></script>
 </body>
 
