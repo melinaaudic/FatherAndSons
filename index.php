12e19942ad3872ea4bdb3ca7d16c783f5d8a5836
melaudic <melinaaudic@MacBook-Pro-de-Melina.local>
2018-08-02 13:19:47 +0700

    Ajout de tout le style + responsive

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
 <body>
   <div class="container-fluid background dark">
     <header class="wrapper">
-      <div class="row middle-lg between-lg">
-        <img src="medias/logo.png" alt="" class="col-lg-4">
-        <p class="col-lg-2">
+      <div class="row middle-sm between-sm">
+        <img src="medias/logo.png" alt="Father and Sons" class="col-sm-8 col-md-4">
+        <p class="col-sm-3">
           Magasin Avignon<br>
           <a href="tel:+33490871175">04.90.87.11.75</a>
         </p>
@@ -25,131 +25,200 @@
     </header>
   </div>
   <main>
-    <section class="page-section container-fluid background image">
+    <section class="page-section container-fluid background offre">
       <div class="wrapper">
-        <div class="row middle-lg center-lg">
-          <h1>Profitez dès aujourd'hui de 15€ de bon d'achat sur votre magasin d'Avignon</h1>
-        </div>
-        <div class="row middle-lg center-lg">
-          <p class="bon-achat">
-            <span>bon d’achat de</span>
-            <span>15€</span>
-            <span>offert*</span>
-          </p>
+        <div class="row middle-sm center-sm">
+          <div class="col-sm-11">
+            <div class="row center-md">
+              <h1 class="col-sm-12 col-lg-9"><strong>Profitez dès aujourd'hui de 15€ de bon d'achat</strong> sur votre magasin <strong>d'Avignon</strong></h1>
+            </div>
+            <div class="row middle-sm center-sm">
+              <p class="col-sm-6 col-md-4 col-lg-3 bon-achat">
+                <span>bon d’achat de</span>
+                <span><strong>15€</strong></span>
+                <span>offert*</span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
     <section class="page-section container-fluid wrapper">
-      <div class="row middle-lg center-lg">
-        <p class="col-lg-4">Inscrivez-vous et profitez d’un bon d’achat de 15€ offert* valable dans votre nouveau magasin d’Avignon en centre ville.</p>
-        <form action="" method="get" class="col-lg-4">
-          <input type="email" name="email" placeholder="Adresse e-mail*" required>
-          <input type="submit" value="J'en profite">
-        </form>
-        <p class="col-lg-4">
-          <span>Plus que</span>
-          <span>168h:17m:02s</span>
-          <span>pour en profiter !</span>
-        </p>
+      <div class="row middle-sm center-sm">
+        <div class="col-md-11 col-lg-4">
+          <div class="row center-sm">
+            <div class="col-sm">
+              <p>Inscrivez-vous et profitez d’un bon d’achat de 15€ offert* valable dans votre nouveau magasin <strong>d’Avignon en centre ville</strong>.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-11 col-md-4">
+          <div class="row">
+            <div class="col-sm">
+              <form action="" method="post" name="inscription">
+                <input type="email" name="email" id="email" placeholder="Adresse e-mail*" required>
+                <input type="submit" value="J'en profite" class="button" id="submit">
+                <p class="hide field_missing" id="email_missing">Veuillez entre votre e-mail</p>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-11 col-md-4">
+          <div class="row center-sm">
+            <div class="col-sm timer">
+              <p>Plus que</p>
+              <p id="timer"></p>
+              <p>pour en profiter !</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
     <section class="page-section container-fluid background dark">
       <div class="wrapper">
-        <div class="row center-lg">
-          <h2 class="col-lg-4">Collections</h2>
+        <div class="row center-sm">
+          <h2 class="col-sm-6">Collections</h2>
         </div>
-        <div class="row center-lg">
-          <p class="col-lg-10">Riche d’une expérience de plus de 20 ans dans la mode homme, chacune de nos collections est conçue pour vous proposer des produits de modes, confectionnées dans des matières sélectionnées pour leur qualité incomparable.</p>
+        <div class="row center-sm">
+          <p class="col-sm-10">Riche d’une expérience de plus de 20 ans dans la mode homme, chacune de nos collections est conçue pour vous proposer des produits de modes, confectionnées dans des matières sélectionnées pour leur qualité incomparable.</p>
         </div>
-        <div class="row  center-lg">
-          <div class="col-lg-4">
-            <h3>Les costumes</h3>
+        <div class="row center-sm vestes">
+          <div class="col-sm-10 col-md-4">
+            <div class="costumes-contain">
+              <h3>Les costumes</h3>
+            </div>
           </div>
-          <div class="col-lg-4">
-            <h3>Le casual</h3>
+          <div class="col-sm-10 col-md-4">
+            <div class="casual-contain">
+              <h3>Le casual</h3>
+            </div>
           </div>
-          <div class="col-lg-4">
-            <h3>Les vestes cuirs</h3>
+          <div class="col-sm-10 col-md-4">
+            <div class="vestes-contain">
+              <h3>Les vestes cuirs</h3>
+            </div>
           </div>
         </div>
       </div>
     </section>
     <section class="page-section container-fluid wrapper">
-      <div class="row center-lg">
-        <h2 class="col-lg-4">Conseils & style</h2>
+      <div class="row center-sm">
+        <h2 class="col-sm-10 col-md-6">Conseils &amp; style</h2>
       </div>
-      <div class="row  center-lg">
-        <div class="col-lg-4">
-          <h3>Conseils pour prendre soin de sa chemise</h3>
-          <a href="#" class="button">découvrir</a>
+      <div class="row center-sm conseil">
+        <div class="col-sm-12 col-md-4">
+          <div class="background dark">
+            <h3>Conseils pour prendre soin de sa chemise</h3>
+            <a href="#" class="button">découvrir</a>
+          </div>
         </div>
-        <div class="col-lg-4">
-          <h3>Comment bien assortir sa cravte à sa chemise ?</h3>
-          <a href="#" class="button">découvrir</a>
+        <div class="col-sm-12 col-md-4">
+          <div class="background dark">
+            <h3>Comment bien assortir sa cravate à sa chemise&nbsp;?</h3>
+            <a href="#" class="button">découvrir</a>
+          </div>
         </div>
-        <div class="col-lg-4">
-          <h3>Comment s'habiller pour un mariage civil ?</h3>
-          <a href="#" class="button">découvrir</a>
+        <div class="col-sm-12 col-md-4">
+          <div class="background dark">
+            <h3>Comment s'habiller pour un mariage civil&nbsp;?</h3>
+            <a href="#" class="button">découvrir</a>
+          </div>
         </div>
       </div>
     </section>
     <section class="page-section container-fluid background light">
       <div class="wrapper">
-        <div class="row center-lg">
-          <h2 class="col-lg-4">Témoignages</h2>
+        <div class="row center-sm">
+          <h2 class="col-sm-6">Témoignages</h2>
+        </div>
+        <div class="row center-sm start-md">
+          <div class="col-md-offset-1 col-md-4 col-sm-10">
+            <blockquote>
+              <p>Riche d’une expérience de plus de 20 ans dans la mode homme, chacune de nos collections est conçue pour vous proposer des produits de modes, confectionnées dans des matières sélectionnées pour leur qualité incomparable.</p>
+              <cite>François F.</cite>
+            </blockquote>
+          </div>
+          <div class="col-md-offset-2 col-md-4 col-sm-10">
+            <blockquote>
+              <p>Riche d’une expérience de plus de 20 ans dans la mode homme, chacune de nos collections est conçue pour vous proposer des produits de modes, confectionnées dans des matières sélectionnées pour leur qualité incomparable.</p>
+              <cite>François F.</cite>
+            </blockquote>
+          </div>
         </div>
       </div>
     </section>
     <section class="page-section container-fluid wrapper">
-      <div class="row center-lg">
-        <h2 class="col-lg-4">Ouverture magasin Avignon</h2>
+      <div class="row center-sm">
+        <h2 class="col-sm-10 col-md-6">Ouverture magasin Avignon</h2>
       </div>
-      <div class="row col-lg-offset-1">
-        <div class="col-lg-4">
-          <h2>Avignon</h2>
-          <p>11 rue Joseph VERNET<br> Avignon <br> 84000 <br> Tel: 04.90.87.11.75</p>
-          <a href="#" class="button">Voir la fiche détaillée</a>
-        </div>
-        <div class="col-lg-6">
-          <p>google maps</p>
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+          <div class="row background light">
+            <div class="col-md-5 col-sm-12">
+              <div class=" magasin">
+                <h3>Avignon</h3>
+                <p>11 rue Joseph VERNET<br> Avignon <br> 84000 <br> Tel: 04.90.87.11.75</p>
+                <a href="#" class="button">Voir la fiche détaillée</a>
+              </div>
+            </div>
+            <div class="col-md-7 col-sm-12">
+              <div class="background">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d304.2608792585906!2d4.8037247174574045!3d43.949814756457116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b5eb85ca855555%3A0x1e53c1f74d0bda72!2sFATHER+%26+SONS+AVIGNON!5e0!3m2!1sfr!2sth!4v1533180177045" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
     <section class="page-section container-fluid background light">
       <div class="wrapper">
-        <div class="row center-lg">
-          <h2 class="col-lg-4">Référénces</h2>
+        <div class="row center-sm">
+          <h2 class="col-sm-6">Référénces</h2>
         </div>
-        <div class="row">
-          <p>images</p>
+        <div class="row around-sm">
+          <div class="col-sm-9 col-md-6 col-lg-12 reference">
+            <div class="row around-sm">
+              <img src="medias/ref-1-bmw.svg" alt="reference bmw">
+              <img src="medias/ref-2-fiat.png" alt="reference fiat">
+              <img src="medias/ref-3-audi.svg" alt="reference audi">
+              <img src="medias/ref-4-nissan.png" alt="reference nissan">
+              <img src="medias/ref-5-mercedez-benz.png" alt="reference mercedez benz">
+              <img src="medias/ref-6-la-reserve.png" alt="reference la réserve">
+              <img src="medias/ref-7-le-meurice.png" alt="reference le meurice">
+              <img src="medias/ref-8-pullman.png" alt="reference pullman">
+              <img src="medias/ref-9-hotel-martinez.png" alt="reference hotel martinez">
+            </div>
+          </div>
         </div>
       </div>
     </section>
     <section class="page-section container-fluid wrapper">
-      <div class="row center-lg">
-        <h2 class="col-lg-4">Vêtement image & Uniforme</h2>
+      <div class="row center-sm">
+        <h2 class="col-sm-10 col-md-6">Vêtement image &amp; Uniforme</h2>
       </div>
-      <div class="row col-lg-offset-1 middle-lg">
-        <div class="col-lg-4">
-          <p>Cultivez votre image de marque avec élégance ! Et si vous adoptiez une image chic et branchée pour séduire votre clientèle ?</p>
-          <p>Father & Sons, plus de 20 ans de savoir-faire dans le prêt-à-porter au service de vos uniformes professionnels.</p>
+      <div class="row middle-sm center-sm vetement">
+        <div class=" col-sm-10 col-md-5 col-lg-offset-1 col-lg-4">
+          <div class="background dark">
+            <p>Cultivez votre image de marque avec élégance ! Et si vous adoptiez une image chic et branchée pour séduire votre clientèle&nbsp;?</p>
+            <p>Father &amp; Sons, plus de 20 ans de savoir-faire dans le prêt-à-porter au service de vos uniformes professionnels.</p>
+          </div>
         </div>
-        <div class="col-lg-3">
+        <div class="col-sm-10 col-md-3">
           <img src="medias/vetement-image-et-uniforme-homme.jpg" alt="">
         </div>
-        <div class="col-lg-3">
+        <div class="col-sm-10 col-md-3">
           <img src="medias/vetement-image-et-uniforme-femme.jpg" alt="">
         </div>
       </div>
     </section>
-
   </main>
-
   <footer class="container-fluid background dark">
-    <div class="row">
-      <div class="col-lg-10">
-        <p>* 15€ Offerts sur dans le magasin d’Avignon uniquement, offre non cumulable.</p>
-        <p>© 2015 Father & Sons et The Other Store - Mentions légales</p>
+    <div class="wrapper">
+      <div class="row center-sm">
+        <div class="col-sm-10">
+          <p>* 15€ Offerts sur dans le magasin d’Avignon uniquement, offre non cumulable.</p>
+          <p>© 2015 Father &amp; Sons et The Other Store - Mentions légales</p>
+        </div>
       </div>
     </div>
   </footer>
--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
-<!DOCTYPE html>
-<html lang="">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
-  <link rel="stylesheet" href="">
-</head>
+// Contact form
+var form = document.querySelector("form");
+var regEx = /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/;
 
-<body>
-  <script src=""></script>
-</body>
-</html>
+function f_valid(e) {
+  for (var i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].type == "text" || form.elements[i].type == "textarea" || form.elements[i].type == "email") {
+      var idMissing = document.querySelector("#" + form.elements[i].id + "_missing");
+
+      if (document.getElementById(form.elements[i].id).value === "") {
+        e.preventDefault();
+        idMissing.classList.remove("hide");
+      } else if (form.elements[i].type == "email" && regEx.test(email.value) == false) {
+        e.preventDefault();
+        idMissing.classList.remove("hide");
+        idMissing.textContent = "Le format de l'email n'est pas valide";
+      } else {
+        idMissing.classList.add("hide");
+      }
+    }
+  }
+}
+
+document.getElementById("submit").addEventListener("click", f_valid);
+
+
+// Compte à rebours
+var deadline = new Date("September 30, 2018 00:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("timer").innerHTML = days + "j " 
++ hours + "h " + minutes + "m " + seconds + "s ";
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
